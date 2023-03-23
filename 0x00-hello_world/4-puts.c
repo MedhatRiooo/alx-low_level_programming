669f8cfd570434d9153970242ce97a39e74078ec
MedhatRiooo <medhatatef2004@gmail.com>
2023-03-23 06:52:48 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * This function is the entry point of the program.
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
-    return 0;
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
